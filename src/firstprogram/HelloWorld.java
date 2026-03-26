0f5e3736f72f716c7aec7fa8f32f844d0f8cd673
vulam2417155 <trinhvulam20@gmail.com>
2026-03-26 14:44:31 +0700

    Complete assignment 2.2.1

---
+++ src/firstprogram/HelloWorld.java
@@ -0,0 +1,8 @@
+package firstprogram;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+    	System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t World!");
+    }
+}
