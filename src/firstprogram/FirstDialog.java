d3715078530cd4ae0ad0f1e1d5e31acec4980797
vulam2417155 <trinhvulam20@gmail.com>
2026-03-26 14:49:50 +0700

    Complete assignment 2.2.2

---
+++ src/firstprogram/FirstDialog.java
@@ -0,0 +1,7 @@
+package firstprogram;
+import javax.swing.JOptionPane;
+	public class FirstDialog {
+	    public static void main(String[] args) {
+	        JOptionPane.showMessageDialog(null, "Hello, World!");
+	    }
+	}
